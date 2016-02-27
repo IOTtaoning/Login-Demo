f62f2809863c8368f0f85ed9caa0c482892ca85e
Apurv <apurvsingh007@gmail.com>
2016-02-27 08:12:29 +0530

    Upload

---
+++ app/src/main/java/com/cgossip/in/login/Login_Activity.java
@@ -0,0 +1,15 @@
+package com.cgossip.in.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dell on 25-02-2016.
+ */
+public class Login_Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
